8bfd6bc8ef1078a7ffb5a7da4a752aa47626c567
kastair <kennaalstair@gmail.com>
2019-11-07 18:11:36 -0800

    changed spelling of 'ingredients'

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ingrediantsRouter = require("./routes/ingrediants/ingrediantsRouter");
+const ingrediantsRouter = require("./routes/ingredients/ingredientsRouter");
 const recipeRouter = require("./routes/recipe/recipeRouter");
 const rIRouter = require("./routes/recipeIngredients/recIngRouter");
 const stepsRouter = require("./routes/steps/stepsRouter");
@@ -7,7 +7,7 @@ const server = express();
 
 server.use(express.json());
 server.use(Logger);
-server.use("/api/ingrediants", ingrediantsRouter);
+server.use("/api/ingredients", ingrediantsRouter);
 server.use("/api/recipes", recipeRouter);
 server.use("/api/recing", recIngRouter);
 server.use("/api/steps", stepsRouter);
